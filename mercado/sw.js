3963205c8280a1128ac3d09c5e01d9cdc09dd776
gabrielcongui95-boop <gabriel.congui95@gmail.com>
2026-07-27 20:39:14 -0300

    Create sw.js

---
+++ mercado/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'mercado-v2';
+const assets = [
+  './Mercado.html',
+  './manifest.json',
+  'https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore-compat.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
