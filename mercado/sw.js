94d08ef39f36c0c289db01160b47f4668068224c
gabrielcongui95-boop <gabriel.congui95@gmail.com>
2026-07-27 21:28:28 -0300

    Update sw.js

--- mercado/sw.js
+++ mercado/sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'mercado-v2';
 const assets = [
-  './mercado.html',
+  './Store.html',
   './manifest.json',
   'https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js',
   'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore-compat.js'
