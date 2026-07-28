89263009a74cf9185dbebbdcee84982635e105bd
gabrielcongui95-boop <gabriel.congui95@gmail.com>
2026-07-28 16:04:47 -0300

    Update app.py

--- monitora/app.py
+++ monitora/app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # --- CONFIGURAÇÃO DA PÁGINA ---
 st.set_page_config(
-    page_title="Monitoramento PDV",
+    page_title="",
     page_icon="🖥️",
     layout="wide",
     initial_sidebar_state="collapsed"  # Melhora a visualização inicial no celular
